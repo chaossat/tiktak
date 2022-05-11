5efe823c04ecf259bf87d8f562d21e1222293c98
chaossat <87358991+chaossat@users.noreply.github.com>
2022-05-11 22:58:53 +0800

    新增登录接口处理器

--- controller/login.go
+++ controller/login.go
@@ -7,7 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-//LoginHandler:登录接口
+//LoginHandler:登录接口处理器
 func LoginHandler(ctx *gin.Context) {
 	username := ctx.Query("username")
 	password := ctx.Query("password")
@@ -27,19 +27,6 @@ func LoginHandler(ctx *gin.Context) {
 		LoginResponse(ctx, -2, "Error Occoured!", 0, "")
 		return
 	}
-	// userVerifyInfo := model.User_verify_Info{}
-	// db.GetUserVerifyInfo(username, &userVerifyInfo)
-	// if userVerifyInfo.ID < 1 {
-	// 	LoginResponse(ctx, -1, "No Such User!", 0, "")
-	// 	return
-	// }
-	// if util.Encodeing(password) != userVerifyInfo.Password {
-	// 	LoginResponse(ctx, -2, "Wrong Password!", 0, "")
-	// 	return
-	// }
-	// userVerifyInfo.ExpirationTime = int(time.Now().Unix()) + 60*60*24 //token过期时间为1天
-	// userVerifyInfo.Token = util.TokenGenerator(username)              //构建新的token
-	// err := db.UserLogin(&userVerifyInfo)
 	err, token := db.CreateToken(&userinfo)
 	if err != nil {
 		fmt.Println(err.Error())
