7e0f644186f937785658d9566f090905082d82b7
ChaosSat <a1b2c3d43902@sina.com>
2022-05-13 09:42:44 +0800

    新增本地URL测试

--- controller/ossURLtest.go
+++ controller/ossURLtest.go
@@ -7,7 +7,10 @@ import (
 
 //测试oss.GetURL
 func GetURL(ctx *gin.Context) {
+	//测试oss链接
 	URL := oss.GetURL("videos/c90b44a96ed080c1a6c8ce8888a40a5aaaa7e7ca.mp4")
+	//测试本地链接
+	// URL := oss.GetURL("tempfile/c90b44a96ed080c1a6c8ce8888a40a5aaaa7e7ca.mp4")
 	ctx.JSON(200, gin.H{
 		"video": URL,
 	})
