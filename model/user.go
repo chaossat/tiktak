6f1ef8955743065164fd1c022fbc46f711eeffb1
paopaoofyjz <yangjinzhao1998@163.com>
2022-05-17 18:58:24 +0800

    添加了关注和点赞的切片，取消了计数字段，查询时通过gorm预加载计数即可

--- model/user.go
+++ model/user.go
@@ -2,9 +2,13 @@ package model
 
 //User_info:用户信息
 type User_info struct {
-	ID             int64 `gorm:"column:id;primary_key;AUTO_INCREMENT;not null"`
-	Name           string
-	Follow_count   int64
-	Follower_count int64
-	Password_Hash  string // 加密后的密码
+	ID   int64 `gorm:"column:id;primary_key;AUTO_INCREMENT;not null"`
+	Name string
+	//Follow_count   int64
+	//Follower_count int64
+	Password_Hash  string      // 加密后的密码
+	Follows        []User_info //该用户的关注
+	Followers      []User_info //该用户的粉丝
+	Videos         []Video     //该用户发布的作品
+	FavoriteVideos []Video     //用户点赞过的作品
 }
--- model/video.go
+++ model/video.go
@@ -2,12 +2,14 @@ package model
 
 //Video:视频信息
 type Video struct {
-	ID             int64 `gorm:"column:id;primary_key;AUTO_INCREMENT;not null"`
-	Title          string
-	AuthorID       int64  //作者id
-	UpdateTime     int64  //发布、更新时间
-	Location       string //储存位置
-	CommentCount   int64  //评论数
-	FavoriteCount  int64  //点赞数
-	Cover_location string //封面储存位置
+	ID         int64 `gorm:"column:id;primary_key;AUTO_INCREMENT;not null"`
+	Title      string
+	AuthorID   int64  //作者id
+	UpdateTime int64  //发布、更新时间
+	Location   string //储存位置
+	//CommentCount   int64     //评论数
+	//FavoriteCount  int64     //点赞数
+	Cover_location string    //封面储存位置
+	PlayLocation   string    //视频播放地址
+	Comments       []Comment //视频的评论
 }
