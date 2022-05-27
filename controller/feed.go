c8f6e0211228fa29daa3416fad22e8f6f88406e1
ChaosSat <a1b2c3d43902@sina.com>
2022-05-27 19:37:11 +0800

    minor patch

--- controller/feed.go
+++ controller/feed.go
@@ -2,7 +2,6 @@ package controller
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"net/http"
 	"strconv"
@@ -14,7 +13,6 @@ import (
 )
 
 func Feed(ctx *gin.Context) {
-	fmt.Println("11111111111111111111111111111111111111")
 	latest_time := ctx.Query("latest_time")
 	latesttime, err := strconv.ParseInt(latest_time, 10, 64)
 	if err != nil {
@@ -56,9 +54,6 @@ func Feed(ctx *gin.Context) {
 			"status_msg":  "调用远程服务错误",
 		})
 		return
-	}
-	for _, j := range resp.VideoList {
-		fmt.Println(j.PlayUrl)
 	}
 	ctx.JSON(http.StatusOK, resp)
 }
--- router/router.go
+++ router/router.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 
 	"github.com/chaossat/tiktak/controller"
-	"github.com/chaossat/tiktak/middleware"
 	"github.com/gin-gonic/gin"
 )
 
@@ -22,7 +21,7 @@ func Init(r *gin.Engine) {
 			user.POST("/register/", controller.Register)
 			user.POST("/login/", controller.LoginHandler)
 		}
-		douyin.Use(middleware.JwtToken())
+		// douyin.Use(middleware.JwtToken())
 		douyin.GET("/osstest", controller.GetURL) //临时测试地址
 		//douyin.GET("/feed", controller.VideoListHandler)
 		publish := douyin.Group("/publish")
