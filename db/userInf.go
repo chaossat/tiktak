7fda1f5a67b8ed2e043a769047977d4e1e271671
ChaosSat <a1b2c3d43902@sina.com>
2022-06-05 12:05:02 +0800

    minor patch

--- db/userInf.go
+++ db/userInf.go
@@ -1,6 +1,9 @@
 package db
 
 import (
+	"errors"
+	"strings"
+
 	"github.com/chaossat/tiktak/common"
 	"github.com/chaossat/tiktak/model"
 	"github.com/chaossat/tiktak/util"
@@ -21,12 +24,18 @@ func UserInfoById(id int) (model.User, error) {
 // 查询用户信息根据用户名
 func UserInfoByName(name string) (error, model.User) {
 	inf := model.User{}
+	if strings.Contains(name, " ") {
+		return errors.New("invalid username"), inf
+	}
 	err := common.GetDB().Where("username=?", name).First(&inf).Error
 	return err, inf
 }
 
 // 用户登录
 func UserLogin(name string, pwd string) (bool, error) {
+	if strings.Contains(name, " ") || strings.Contains(pwd, " ") {
+		return false, errors.New("invalid username or password")
+	}
 	p := 0
 	// 密码做MD5
 	pwd_md5 := util.MD5V(pwd)
@@ -36,6 +45,9 @@ func UserLogin(name string, pwd string) (bool, error) {
 
 // 用户注册
 func UserInfoRegister(name string, pwd string) error {
+	if strings.Contains(name, " ") || strings.Contains(pwd, " ") {
+		return errors.New("invalid username or password")
+	}
 	// 密码做md5
 	pwd_md5 := util.MD5V(pwd)
 	inf := model.User{
